1b677cb472846b83c73d26c66d5da41048b660c1
Durucz Béla <30567460+the-developer-guy@users.noreply.github.com>
2025-08-27 21:05:43 +0200

    Added unit test for Lobby marshaling

---
+++ internal/server/lobby_test.go
@@ -0,0 +1,18 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestLobbyJson(t *testing.T) {
+	l := NewLobby("aToken", "bToken")
+	jsonData, err := json.Marshal(l)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	fmt.Printf("Lobby JSON: %s", string(jsonData))
+}
