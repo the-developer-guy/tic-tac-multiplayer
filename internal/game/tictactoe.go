40036246c00dd2f7430bbb85c378a25bd25ccbb5
Durucz Béla <30567460+the-developer-guy@users.noreply.github.com>
2025-09-24 21:57:27 +0200

    Implemented mark placement in t3 grid

--- internal/game/tictactoe.go
+++ internal/game/tictactoe.go
@@ -1,6 +1,10 @@
 package game
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"math/rand/v2"
+)
 
 type TicTacToeMark struct {
 	X int `json:"x"`
@@ -27,14 +31,50 @@ func NewTicTacToeGrid() *TicTacToeGrid {
 	return &tttg
 }
 
-func (tttg *TicTacToeGrid) PlaceXMark(x, y int) error {
-	return errors.New("not implemented")
+func (tttg *TicTacToeGrid) PlaceMark(x, y int, mark Mark) error {
+
+	if tttg.Field[x][y] != MarkEmpty {
+		return fmt.Errorf("%d;%d position is already filled", x, y)
+	}
+
+	m := TicTacToeMark{x, y}
+
+	switch mark {
+	case MarkX:
+		tttg.XMarks = append(tttg.XMarks, m)
+	case MarkO:
+		tttg.OMarks = append(tttg.OMarks, m)
+	case MarkEmpty:
+		return errors.New("can't place empty mark")
+	default:
+		return errors.New("invalid mark")
+	}
+
+	tttg.Field[x][y] = mark
+
+	return nil
 }
 
-func (tttg *TicTacToeGrid) PlaceOMark(x, y int) error {
-	return errors.New("not implemented")
-}
+func (tttg *TicTacToeGrid) PlaceRandomMark(mark Mark) error {
 
-func (tttg *TicTacToeGrid) PlaceRandomOMark() error {
-	return errors.New("not implemented")
+	freePlaces := [9]TicTacToeMark{}
+	freeCount := 0
+
+	for x, row := range tttg.Field {
+		for y, cell := range row {
+			if cell == MarkEmpty {
+				freePlaces[freeCount].X = x
+				freePlaces[freeCount].Y = y
+				freeCount++
+			}
+		}
+	}
+	if freeCount == 0 {
+		return errors.New("game field is full")
+	}
+
+	r := rand.IntN(freeCount)
+	m := freePlaces[r]
+
+	return tttg.PlaceMark(m.X, m.Y, mark)
 }
