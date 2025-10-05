71ca4aa0bd3d12236d079dc65e43741b699a1a92
Durucz Béla <30567460+the-developer-guy@users.noreply.github.com>
2025-10-05 20:25:19 +0200

    Added Players management skeleton

--- internal/auth/playerAuth.go
+++ internal/auth/playerAuth.go
@@ -1,10 +1,23 @@
 package auth
 
+import "errors"
+
 type PlayerAuth struct {
+	Players map[string]*Player // ID-Player
 }
 
 func NewPlayerAuth() *PlayerAuth {
-	pa := PlayerAuth{}
+	pa := PlayerAuth{
+		Players: make(map[string]*Player),
+	}
 
 	return &pa
+}
+
+func (pa *PlayerAuth) AddPlayer(id string, player *Player) error {
+	return errors.New("not implemented")
+}
+
+func (pa *PlayerAuth) LoadPlayers() error {
+	return errors.New("not implemented")
 }
