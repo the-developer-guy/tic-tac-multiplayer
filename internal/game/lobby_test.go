224b0588005381809287a6439024e7344d2fb965
Durucz Béla <30567460+the-developer-guy@users.noreply.github.com>
2025-10-05 21:47:45 +0200

    Updated test

--- internal/game/lobby_test.go
+++ internal/game/lobby_test.go
@@ -4,10 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestLobbyJson(t *testing.T) {
-	l := NewLobby("aToken", "bToken")
+	l := NewLobby("aToken", "bToken", 1, 2, time.Now())
 	jsonData, err := json.Marshal(l)
 	if err != nil {
 		t.Error(err)
